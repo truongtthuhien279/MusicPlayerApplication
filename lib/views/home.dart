b49f792e4420c002998e0a88c28926a222836f26
truongtthuhien279 <truongtthuhien@gmail.com>
2024-02-27 00:15:45 +0700

    get music from storage

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:music_player/consts/text_style.dart';
 import 'package:music_player/consts/colors.dart';
+import 'package:music_player/controllers/player_controller.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var controller = Get.put(PlayerController());
+
     return Scaffold(
-      backgroundColor: bgDarkColor,
+        backgroundColor: bgDarkColor,
         appBar: AppBar(
           backgroundColor: bgDarkColor,
           actions: [
@@ -31,32 +37,67 @@ class Home extends StatelessWidget {
             ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            physics: BouncingScrollPhysics(),
-            itemCount: 100,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                margin: EdgeInsets.only(bottom: 4),
-                  child: ListTile(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)
-                    ),
-
-                    tileColor: bgColor,
-                      title: Text(
-                    "Music Name",
-                    style: ourStyle(family: bold, size: 15)),
-                  subtitle:Text(
-                    "Artist Name",
-                    style: ourStyle(family: regular, size: 12)),
-                    leading: Icon(Icons.music_note, color: whiteColor, size: 32,),
-                    trailing: Icon(Icons.play_arrow, color: whiteColor, size: 26,),
-                  )
-              );
-            },
-          ),
-        ));
+        body: FutureBuilder<List<SongModel>>(
+            future: controller.audioQuery.querySongs(
+              ignoreCase: true,
+              orderType: OrderType.ASC_OR_SMALLER,
+              sortType: null,
+              uriType: UriType.EXTERNAL,
+            ),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.data == null) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              else if (snapshot.data!.isEmpty) {
+                return  Center(
+                  child: Text(
+                      "No song found",
+                    style: ourStyle(),
+                  ),
+                );}
+              else {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                          margin: EdgeInsets.only(bottom: 4),
+                          child: Obx(
+                            () => ListTile(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              tileColor: bgColor,
+                              title: Text(
+                                  snapshot.data![index].displayNameWOExt,
+                                  style: ourStyle(family: bold, size: 15)),
+                              subtitle: Text("${snapshot.data![index].artist}",
+                                  style: ourStyle(family: regular, size: 12)),
+                              leading: QueryArtworkWidget(
+                                id: snapshot.data![index].id,
+                                type: ArtworkType.AUDIO,
+                                nullArtworkWidget: const Icon(
+                                  Icons.music_note,
+                                  color: whiteColor,
+                                  size: 32,
+                                ),
+                              ),
+                              trailing: controller.playIndex.value == index &&
+                                      controller.isPlaying.value
+                                  ? Icon(Icons.play_arrow,
+                                      color: whiteColor, size: 26)
+                                  : null,
+                              onTap: () {
+                                controller.playSong(
+                                    snapshot.data![index].uri, index);
+                              },
+                            ),
+                          ));
+                    },
+                  ),
+                );
+              }
+            }));
   }
 }
